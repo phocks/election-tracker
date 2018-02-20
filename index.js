9fc4cfa2783e4ee1a1cee39b71a39de6d0455f20
Joshua Byrd <phocks@gmail.com>
2018-02-20 16:57:25 +1000

    Testing out this watcher

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const axios = require("axios");
+var Watcher = require("feed-watcher"),
+  feed = "http://www.abc.net.au/news/feed/51120/rss.xml",
+  interval = 10; // seconds
 
-const url =
-  "https://maker.ifttt.com/trigger/trigger/with/key/c_p7feEXVl6AVO6YDdyY9f";
+// if not interval is passed, 60s would be set as default interval.
+var watcher = new Watcher(feed, interval);
 
-const triggerEvent = async url => {
-  const response = await axios({
-    method: "post",
-    url: url,
-    data: { value1: "1", value2: "2", value3: "3" }
-  }).catch(error => console.log(error));
-  const data = response.data;
-  console.log(data);
-};
+// Check for new entries every n seconds.
+watcher.on("new entries", function(entries) {
+  entries.forEach(function(entry) {
+    console.log(entry.title);
+  });
+});
 
-triggerEvent(url);
+// Start watching the feed.
+watcher
+  .start()
+  .then(function(entries) {
+    console.log(entries);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
