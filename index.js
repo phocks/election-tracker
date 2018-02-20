fa84c49b40693a7be17aec3500b97c07e8cd13c7
Joshua Byrd <phocks@gmail.com>
2018-02-20 15:17:08 +1000

    OK forget it IFTTT only allows 15 min updates anyway

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Write something");
+const axios = require("axios");
+
+const url =
+  "https://maker.ifttt.com/trigger/trigger/with/key/c_p7feEXVl6AVO6YDdyY9f";
+
+const triggerEvent = async url => {
+  const response = await axios({
+    method: "post",
+    url: url,
+    data: { value1: "1", value2: "2", value3: "3" }
+  }).catch(error => console.log(error));
+  const data = response.data;
+  console.log(data);
+};
+
+triggerEvent(url);
